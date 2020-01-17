479cb94d1e351d35b5818c51c54899fa2e1d0909
Andrea Bottini <andrea.bottini22@gmail.com>
2020-01-17 17:23:19 +0100

    Impostazione prima parte esercizio

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// Partendo dai file html e css che vi allego, implementate la funzionalità slider.
+// Se clicchiamo sulle icone next e prev andiamo avanti o indietro con le visualizzazioni. Il giro deve ripartire se siamo sull'ultima o la prima.
+// I cerchietti devono cambiare colore contemporaneamente.
+
+
+// preparo il documento per il controllo dello script e seleziono le classi necessarie
+$(document).ready(
+  function () {
+    $('.next').click(
+      function () {
+        nextFoto();
+      }
+    );
+    $('.prev').click(
+      function () {
+        prevFoto();
+      }
+    );
+  }
+);
+
+// Creo le funzioni da inserire
+function nextFoto() {
+  alert('nextFoto');
+}
+
+function prevFoto() {
+  alert('prev Foto');
+}
