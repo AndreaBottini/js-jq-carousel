639ffbe26d17ff53adcd1e28b1564888cb54a566
Andrea Bottini <andrea.bottini22@gmail.com>
2020-01-17 17:55:47 +0100

    Impostzione slider Back

--- js/main.js
+++ js/main.js
@@ -25,14 +25,29 @@ function nextFoto() {
   var actualImage = $('img.active');
   // utilizzo .next per selezionare immagine successiva
   var imageSucc = actualImage.next();
-  console.log(imageSucc);
-
+  // console.log(imageSucc);
   // rimozione classe active
-  actualImage.removeClass('active');
-
-  console.log(actualImage);
+  if (actualImage.hasClass('last') == true) {
+    actualImage.removeClass('active');
+    $('img.first').addClass('active');
+  } else {
+    actualImage.removeClass('active');
+    imageSucc.addClass('active');
+  }
 }
-
+// imposto anche la parte Previous foto nella funzione
 function prevFoto() {
   // alert('prevFoto');
+  var actualImage = $('img.active');
+  // utilizzo .next per selezionare immagine successiva
+  var imagePrev = actualImage.prev();
+  // console.log(imagePrev);
+  // rimozione classe active
+  if (actualImage.hasClass('first') == true) {
+    actualImage.removeClass('active');
+    $('img.last').addClass('active');
+  } else {
+    actualImage.removeClass('active');
+    imagePrev.addClass('active');
+  }
 }
